23a1747c9eabe0536b2a761a7a156f4930dbabb5
ameziel <samuel.sicard.it@gmail.com>
2023-03-06 23:35:09 +0100

    réorganisation des routes et ajout pour les composants liés à la formule cours

--- src/app/tms/tms.routing.ts
+++ src/app/tms/tms.routing.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import {ElevesComponent} from "./eleve/eleves/eleves.component";
-import {FormulesComponent} from "./formules/formules.component";
+import {FormulesComponent} from "./formule/formules/formules.component";
 import {GroupesComponent} from "./groupes/groupes.component";
 import {MoniteursComponent} from "./moniteurs/moniteurs.component";
 import {PrestationsComponent} from "./prestations/prestations.component";
 import {AjouterEleveComponent} from "./eleve/ajouter-eleve/ajouter-eleve.component";
 import {EleveDetailsComponent} from "./eleve/eleve-details/eleve-details.component";
 import {LoginComponent} from "./login/login.component";
+import {AjouterFormuleComponent} from "./formule/ajouter-formule/ajouter-formule.component";
+import {FormuleDetailsComponent} from "./formule/formule-details/formule-details.component";
 
 
 export const ComponentsRoutes: Routes = [
@@ -21,14 +23,34 @@ export const ComponentsRoutes: Routes = [
         path: "",
         component: LoginComponent
       },
+      // ===================== ELEVES =====================
 			{
 				path: 'eleves',
 				component: ElevesComponent
 			},
+      {
+        path: 'ajouter-eleve',
+        component: AjouterEleveComponent
+      },
+      {
+        path: 'eleve-details/:id',
+        component: EleveDetailsComponent
+      },
+      // ===================================================
+      // ===================== FORMULES =====================
 			{
 				path: 'formules',
 				component: FormulesComponent
 			},
+      {
+        path: 'ajouter-formule',
+        component: AjouterFormuleComponent
+      },
+      {
+        path: 'formule-details/:id',
+        component: FormuleDetailsComponent
+      },
+      // ===================================================
 			{
 				path: 'groupes',
 				component: GroupesComponent
@@ -41,14 +63,8 @@ export const ComponentsRoutes: Routes = [
         path: 'prestations',
         component: PrestationsComponent
       },
-      {
-        path: 'ajouter-eleve',
-        component: AjouterEleveComponent
-      },
-      {
-        path: 'eleve-details/:id',
-        component: EleveDetailsComponent
-      },
+
+
 
 
 		]
