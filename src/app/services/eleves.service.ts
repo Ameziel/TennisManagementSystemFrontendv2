eff78e50e4face1b5d83fc2ac9b2afe96517e23b
ameziel <samuel.sicard.it@gmail.com>
2023-03-31 15:02:41 +0200

    Clean

--- src/app/services/eleves.service.ts
+++ src/app/services/eleves.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Eleve} from "../models/eleve.model";
@@ -9,30 +9,31 @@ import {environment} from "../../environments/environment";
 })
 export class ElevesService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  public getAllEleves():Observable<Array<Eleve>> {
+  public getAllEleves(): Observable<Array<Eleve>> {
     return this.http.get<Array<Eleve>>(environment.backendHost + "/eleves")
   }
 
-  public searchElevesByName(keyword : string):Observable<Array<Eleve>> {
-    return this.http.get<Array<Eleve>>( environment.backendHost+ "/eleves/search?keyword=" + keyword)
+  public searchElevesByName(keyword: string): Observable<Array<Eleve>> {
+    return this.http.get<Array<Eleve>>(environment.backendHost + "/eleves/search?keyword=" + keyword)
   }
 
-  public getEleveById(id : string) : Observable<Eleve> {
+  public getEleveById(id: string): Observable<Eleve> {
     return this.http.get<Eleve>(environment.backendHost + "/eleves/" + id);
   }
 
-  public updateEleve(eleve : Eleve) : Observable<Eleve> {
-    return this.http.put<Eleve>( environment.backendHost + "/eleves/" + eleve.id, eleve);
+  public updateEleve(eleve: Eleve): Observable<Eleve> {
+    return this.http.put<Eleve>(environment.backendHost + "/eleves/" + eleve.id, eleve);
   }
 
-  public saveEleve(eleve : Eleve):Observable<Eleve> {
-    return this.http.post<Eleve>( environment.backendHost + "/eleves", eleve);
+  public saveEleve(eleve: Eleve): Observable<Eleve> {
+    return this.http.post<Eleve>(environment.backendHost + "/eleves", eleve);
   }
 
-  public deleteEleve(id : string){
-     return this.http.delete( environment.backendHost + "/eleves/" + id);
+  public deleteEleve(id: string) {
+    return this.http.delete(environment.backendHost + "/eleves/" + id);
   }
 }
 
